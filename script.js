1e8b81b6ffdd0c9d5ebbc6ba30cf86573901547c
Sameer <sk13042001@gmail.com>
2023-07-13 20:29:00 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
+});
+
+$(".scroll-up-btn").click(function () {
+  $("html").animate({ scrollTop: 0 });
+});
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function openTab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sideMenu = document.getElementById("side-menu");
+
+function openMenu() {
+  sideMenu.style.right = "0";
+}
+
+function closeMenu() {
+  sideMenu.style.right = "-200px";
+}
+var typed = new Typed(".typing", {
+  strings: ["Developer", "Engineer", "Student", "Programmer"],
+  typedSpeed: 80,
+  backSpeed: 80,
+  loop: true,
+});
+var typed = new Typed(".typing-2", {
+  strings: ["Developer", "Engineer", "Student", "Programmer"],
+  typedSpeed: 80,
+  backSpeed: 80,
+  loop: true,
+});
